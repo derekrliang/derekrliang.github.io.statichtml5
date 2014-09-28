01312611eaf10728075d4e6add6e7fe5cae8c808
Derek Liang <frostyrusty@gmail.com>
2014-09-27 23:29:11 -0700

    separating splash, about, projects, .. contacts pages, cleaned up splash spec slider

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.spec-tabs a').on('click', function(e){
+		e.preventDefault();
+		
+		var index = $(this).index();
+		
+		if ($('.spec-tabs').attr('data-tab') == index) {
+			$('.spec-tabs').attr('data-tab', -1);
+			$('.spec-content').attr('data-tab', -1);
+		} else {
+			$('.spec-tabs').attr('data-tab', index);
+			$('.spec-content').attr('data-tab', index);
+		}
+	});
+});
--- js/script.js
+++ js/script.js
@@ -1,60 +1,31 @@
-$( document ).ready(function() {
-var devTCMap = {};
-var devTitles = document.getElementsByClassName('dev-title');
-var devContents = document.getElementsByClassName('dev-content');
-var navMenu = $('#menu');
-
-for (var i = 0; i < devTitles.length; i++) {
-	devTCMap[devTitles[i].getAttribute("hash")] = devContents[i];
-	devTitles[i].addEventListener('click', function() {
-		if (devTCMap[this.getAttribute("hash")].classList.contains("hide-content")) {
-			// Reset the other ones
-			for (var j = 0; j < devTitles.length; ++j) {
-				var title = devTitles[j];
-				if (title == this) {
-					this.className = "dev-title dev-title-active";
-					devTCMap[this.getAttribute("hash")].className = "dev-content show-content"
-				}
-				else {
-					title.className = "dev-title";
-					devTCMap[title.getAttribute("hash")].className = "dev-content hide-content"
-				}
-			}
+$(document).ready(function() {
+	
+	
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+			$('html,body').animate({
+			  scrollTop: target.offset().top
+			}, 750);
+			return false;
+		  }
 		}
-		else if (devTCMap[this.getAttribute("hash")].classList.contains("show-content")) {
-			this.className = "dev-title";
-			devTCMap[this.getAttribute("hash")].className = "dev-content hide-content";
-		}
+	  });
 	});
-}
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 750);
-        return false;
-      }
-    }
-  });
-});
 
 
-$(function() {
-  $('i.fa-bars').click(function() {
-	  if (this.classList.contains("on")) {
-		  this.className = "fa fa-bars";
-		  navMenu.hide();
+  $('.fa-bars').click(function() {
+	  if ($(this).hasClass('on')) {
+		  $('.menu').removeClass('show');
+		  $(this).removeClass('on');
 	  } else {
-		  this.className += " on";
-		  navMenu.show();
+		  $('.menu').addClass('show');
+		  $(this).addClass('on');
 	  }
   });
-});
-
-
+  
+  
 });
